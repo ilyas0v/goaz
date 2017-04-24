6032f7fccb5b3fa283766dc4d874a65cedf157cd
ilyas <ilyas.ilyasov.1@gmail.com>
2017-04-24 21:56:17 +0400

    .

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::group(["middleware"=>["web"]], function(){
 
-Route::get('/', function () { return view('pages.index'); });
-Route::get('haqqimizda',function(){ return view('pages.about'); });
-Route::get('elaqe',function(){ return view('pages.contact');});
-Route::get('giris',function(){return view('auth.giris');});
-Route::get('qeydiyyat',function(){return view('auth.qeydiyyat');});
+    Route::get('/', function () { return view('pages.index'); });
+    Route::get('haqqimizda',function(){ return view('pages.about'); });
+    Route::get('elaqe',function(){ return view('pages.contact');});
+    Route::get('giris',function(){return view('auth.giris');});
+    Route::get('qeydiyyat',function(){return view('auth.qeydiyyat');});
+    // Login ,Logout ve Qeydiyyat
+    Route::post('girish','Auth\LoginController@login');
+    Route::get('cixis',['as'=>'logout' , 'uses' => 'Auth\LoginController@logout']);
+    Route::post('qeydiyyat','Auth\RegisterController@register');
+
+});

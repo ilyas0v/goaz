82a79dc71d435ac57fdd691a2a1c0a3d7c48d008
ilyas <ilyas.ilyasov.1@gmail.com>
2017-05-10 16:12:08 +0400

    .

--- resources/lang/az/validation.php
+++ resources/lang/az/validation.php
@@ -82,7 +82,7 @@ return [
     ],
     'string'               => 'The :attribute must be a string.',
     'timezone'             => 'The :attribute must be a valid zone.',
-    'unique'               => 'The :attribute has already been taken.',
+    'unique'               => ':attribute artiq istifade olunub, bashqa shey yazin',
     'uploaded'             => 'The :attribute failed to upload.',
     'url'                  => 'The :attribute format is invalid.',
 
